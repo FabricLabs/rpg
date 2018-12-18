fe8c3207d19a6753b022cd64234650b655f448a2
Eric Martindale <eric@ericmartindale.com>
2018-12-17 23:29:27 -0800

    Remove compiled file, update more of index.html

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 
 module.exports = {
   entry: './components/index.js',
+  mode: 'development',
   output: {
     filename: 'index.js',
     path: path.resolve(__dirname, 'assets')
